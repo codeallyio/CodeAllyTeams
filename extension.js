c709ed6f0fe7f380c12435e005e56b033ce105cc
adamzaczek <adamzaczekul@gmail.com>
2020-04-01 18:31:32 +0200

    Register hover position

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,29 +9,47 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "stroveteams" is now active!!!!!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "stroveteams" is now active!');
+  // const editor = vscode.window.activeTextEditor;
+  // const position = editor.selection.active;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+  vscode.languages.registerHoverProvider("*", {
+    provideHover(document, position, token) {
+      console.log("hovered position", position);
+      return new vscode.Hover("I am a hover!");
+    }
+  });
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+  //   var newPosition = position.with(position.line, 0);
+  //   var newSelection = new vscode.Selection(newPosition, newPosition);
 
-	context.subscriptions.push(disposable);
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "extension.helloWorld",
+    function() {
+      // The code you place here will be executed every time your command is executed
+
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello World!");
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
+
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate
+};
