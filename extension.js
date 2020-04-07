803173bf5c9a0c78d34527075fc4992cb0b40eea
adamzaczek <adamzaczekul@gmail.com>
2020-04-08 01:07:16 +0200

    Stash changes

--- extension.js
+++ extension.js
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 const fetch = require("node-fetch");
+const axios = require("axios").default;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -45,14 +46,29 @@ function activate(context) {
         y: position.line,
       };
 
-      fetch(localEndpoint, {
-        method: "POST",
-        referrerPolicy: "unsafe-url",
-      })
+      axios
+        .post(localEndpoint, {
+          // referrerPolicy: "unsafe-url",
+          // headers: {
+          //   referrerPolicy: "unsafe-url",
+          // },
+
+          credentials: "include",
+          referrerPolicy: "unsafe-url",
+        })
         .then(function (response) {
           console.log("response", response);
         })
         .catch((error) => console.log("error", error));
+
+      // fetch(localEndpoint, {
+      //   method: "POST",
+      //   referrerPolicy: "unsafe-url",
+      // })
+      //   .then(function (response) {
+      //     console.log("response", response);
+      //   })
+      //   .catch((error) => console.log("error", error));
 
       // console.log("dataToSend", dataToSend);
       // console.log(
