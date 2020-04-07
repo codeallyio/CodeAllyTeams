edc7ff0c7aa4e6023d73df80b197e5e662bd894c
adamzaczek <adamzaczekul@gmail.com>
2020-04-07 22:45:02 +0200

    Add initial api call to extension

--- extension.js
+++ extension.js
@@ -1,9 +1,18 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
+const fetch = require("node-fetch");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+
+const localEndpoint = "http://localhost:3000/liveshareActivity";
+
+fetch(localEndpoint)
+  .then(function (response) {
+    console.log("response", response);
+  })
+  .catch((error) => console.log("error", error));
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -35,7 +44,7 @@ function activate(context) {
       const dataToSend = {
         fileName: document.fileName,
         x: position.character,
-        y: position.line
+        y: position.line,
       };
 
       // console.log("dataToSend", dataToSend);
@@ -48,15 +57,25 @@ function activate(context) {
       //   token
       // );
       // return new vscode.Hover("I am a hover!");
-    }
+    },
   });
+
+  // fetch("http://example.com/movies.json")
+  //   .then((response) => {
+  //     console.log("response", response);
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     console.log("data", data);
+  //   })
+  //   .catch((error) => console.log("error", error));
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     "extension.helloWorld",
-    function() {
+    function () {
       // The code you place here will be executed every time your command is executed
 
       // Display a message box to the user
@@ -74,7 +93,7 @@ function deactivate() {}
 
 module.exports = {
   activate,
-  deactivate
+  deactivate,
 };
 
 /*
@@ -92,16 +111,16 @@ const projects = {
       documentUrl: "/src/assets/index.html",
       position: {
         x: 11,
-        y: 2
-      }
+        y: 2,
+      },
     },
     user2Id: {
       documentUrl: "/package.json",
       position: {
         x: 1,
-        y: 1
-      }
-    }
+        y: 1,
+      },
+    },
   },
   // projectId
   project456Id: {
@@ -109,8 +128,8 @@ const projects = {
       documentUrl: "/index.ts",
       position: {
         x: 1,
-        y: 1
-      }
-    }
-  }
+        y: 1,
+      },
+    },
+  },
 };
