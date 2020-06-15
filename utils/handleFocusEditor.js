9e37461f2154646638b5f996dc5f5af0dcb28463
Piotr Jarosz <jarosz.piotr91@gmail.com>
2020-06-15 20:18:45 +0200

    Added scroll to cursor

--- utils/handleFocusEditor.js
+++ utils/handleFocusEditor.js
@@ -1,12 +1,24 @@
 const vscode = require("vscode");
 
-const handleFocusEditor = ({ uri }) => {
+const handleFocusEditor = async ({ uri, userPosition }) => {
   try {
     const editorPath = vscode.Uri.file(uri);
 
-    vscode.window.showTextDocument(editorPath);
+    const editor = await vscode.window.showTextDocument(editorPath);
 
-    // TextEditor.revealRange
+    const teastPos = {
+      startLine: 100,
+      endLine: 102,
+      startCharacter: 2,
+      endCharacter: 5,
+    };
+
+    const range = new vscode.Range(
+      new vscode.Position(userPosition.startLine, userPosition.startCharacter),
+      new vscode.Position(userPosition.endLine, userPosition.endCharacter)
+    );
+
+    editor.revealRange(range, 1);
   } catch (e) {
     console.log(`Error in handleFocusEditor: ${e}`);
   }
