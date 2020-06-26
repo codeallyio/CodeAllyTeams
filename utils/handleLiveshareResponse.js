2e4fff69518ebd86b454c733976c14019d80b9fa
Piotr Jarosz <jarosz.piotr91@gmail.com>
2020-06-26 18:45:51 +0200

    Fix and version bump

--- utils/handleLiveshareResponse.js
+++ utils/handleLiveshareResponse.js
@@ -64,7 +64,7 @@ const handleLiveshareResponse = (userDataArray) => {
 
       const editor = vscode.window.activeTextEditor;
       const isEditorPathTheSameAsUsers =
-        editor._documentData._uri.path === userData.documentPath;
+        editor?._documentData?._uri?.path === userData?.documentPath;
 
       if (isEditorPathTheSameAsUsers) {
         const codeDecorationType = createDecorationType({
