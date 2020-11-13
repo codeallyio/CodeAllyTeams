dd491f8040481c985653963a0b75565abdab6273
Piotr Jarosz <jarosz.piotr91@gmail.com>
2020-11-13 13:37:09 +0100

    quickFix

--- utils/debugger.js
+++ utils/debugger.js
@@ -13,7 +13,7 @@ const startDebugging = async () => {
       debugMode = true;
     },
     close: () => {
-      debugMode - false;
+      debugMode = false;
     },
     handleInput: async () => {
       // disabling inputs
