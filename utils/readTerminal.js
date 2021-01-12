41159b367f969462d32ae909ba71dccc307a8c62
Piotr Jarosz <jarosz.piotr91@gmail.com>
2021-01-12 18:54:52 +0100

    Quick change

--- utils/readTerminal.js
+++ utils/readTerminal.js
@@ -59,7 +59,7 @@ const readTerminal = async () => {
           ) {
             STARTING_TERMINAL = true;
 
-            terminal = vscode.window.createTerminal("Candidate's preview");
+            terminal = vscode.window.createTerminal("ReadOnly Candidate");
 
             let whileCounter = 0;
 
