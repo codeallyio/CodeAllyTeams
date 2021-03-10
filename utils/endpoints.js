791b218e0bc004bd3072f78a77d3aaa8c4456bae
Piotr Jarosz <jarosz.piotr91@gmail.com>
2021-03-10 17:56:47 +0100

    fix

--- utils/endpoints.js
+++ utils/endpoints.js
@@ -4,17 +4,17 @@ let endpoint;
 let websocketEndpoint;
 let idleTimeout = process.env.STROVE_IDLE_TIMEOUT;
 
-// if (environment === "local" || !environment) {
+if (environment === "local" || !environment) {
   idleTimeout = 5000;
   endpoint = "http://localhost:4040";
   websocketEndpoint = "ws://localhost:4040/graphql";
-// } else if (environment === "development") {
-//   endpoint = "https://graphql.codeally.io";
-//   websocketEndpoint = "wss://graphql.codeally.io/graphql";
-// } else {
-//   endpoint = "https://api.codeally.io/graphql";
-//   websocketEndpoint = "wss://api.codeally.io/graphql";
-// }
+} else if (environment === "development") {
+  endpoint = "https://graphql.codeally.io";
+  websocketEndpoint = "wss://graphql.codeally.io/graphql";
+} else {
+  endpoint = "https://api.codeally.io/graphql";
+  websocketEndpoint = "wss://api.codeally.io/graphql";
+}
 
 const liveshareActivityEndpoint = `${endpoint}/liveshareActivity`;
 const graphqlEndpoint = `${endpoint}/graphql`;
