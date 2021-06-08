3a4e81e89d430e42fc72a9be7987aff06fa885f8
Piotr Jarosz <jarosz.piotr91@gmail.com>
2021-06-08 19:07:03 +0200

    stash

--- utils/terminalSharing.js
+++ utils/terminalSharing.js
@@ -79,6 +79,141 @@ const ActiveUsersTreeDataProvider = (fieldNames) => {
   };
 };
 
+const SharingManagementWebview = () => {
+  let _view;
+
+  let _extensionUri = vscode.Uri.parse(
+    "/Users/mac/Desktop/SiliSky/extension/stroveTeams"
+  );
+
+  const resolveWebviewView = (webviewView, context, _token) => {
+    _view = webviewView;
+
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
+
+      localResourceRoots: [_extensionUri],
+    };
+
+    webviewView.webview.html = _getHtmlForWebview(webviewView.webview);
+    // webviewView.webview.html = `<p>Testing</p>`;
+
+    webviewView.webview.onDidReceiveMessage((data) => {
+      // To DO later
+      // switch (data.type) {
+      // 	case 'colorSelected':
+      // 		{
+      // 			vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
+      // 			break;
+      // 		}
+      // }
+    });
+  };
+
+  // addColor() {
+  // 	if (this._view) {
+  // 		this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
+  // 		this._view.webview.postMessage({ type: 'addColor' });
+  // 	}
+  // }
+
+  // clearColors() {
+  // 	if (this._view) {
+  // 		this._view.webview.postMessage({ type: 'clearColors' });
+  // 	}
+  // }
+
+  const _getHtmlForWebview = (webview) => {
+    try {
+      console.log("elo", _extensionUri);
+      // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+      const scriptUri = webview.asWebviewUri(
+        vscode.Uri.joinPath(_extensionUri, "media", "main.js")
+      );
+      console.log(
+        "🚀 ~ file: terminalSharing.js ~ line 129 ~ SharingManagementWebview ~ scriptUri",
+        scriptUri
+      );
+
+      // Do the same for the stylesheet.
+      const styleResetUri = webview.asWebviewUri(
+        vscode.Uri.joinPath(_extensionUri, "media", "reset.css")
+      );
+      console.log(
+        "🚀 ~ file: terminalSharing.js ~ line 134 ~ SharingManagementWebview ~ styleResetUri",
+        styleResetUri
+      );
+      const styleVSCodeUri = webview.asWebviewUri(
+        vscode.Uri.joinPath(_extensionUri, "media", "vscode.css")
+      );
+      console.log(
+        "🚀 ~ file: terminalSharing.js ~ line 138 ~ SharingManagementWebview ~ styleVSCodeUri",
+        styleVSCodeUri
+      );
+      const styleMainUri = webview.asWebviewUri(
+        vscode.Uri.joinPath(_extensionUri, "media", "main.css")
+      );
+      console.log(
+        "🚀 ~ file: terminalSharing.js ~ line 142 ~ SharingManagementWebview ~ styleMainUri",
+        styleMainUri
+      );
+
+      // Use a nonce to only allow a specific script to be run.
+      const nonce = getNonce();
+
+      return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+
+				<!--
+					Use a content security policy to only allow loading images from https or from our extension directory,
+					and only allow scripts that have a specific nonce.
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+				<link href="${styleResetUri}" rel="stylesheet">
+				<link href="${styleVSCodeUri}" rel="stylesheet">
+				<link href="${styleMainUri}" rel="stylesheet">
+				
+				<title>Cat Colors</title>
+			</head>
+			<body>
+				<ul class="color-list">
+				</ul>
+
+				<button class="add-color-button">Add Color</button>
+
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+			</html>`;
+    } catch (e) {
+      console.log("error: ", e);
+    }
+  };
+
+  return {
+    resolveWebviewView,
+    _getHtmlForWebview,
+    viewType: "sharingManagement",
+    _view,
+    _extensionUri,
+  };
+};
+
+function getNonce() {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
 const registerCommands = (context) => {
   context.subscriptions.push(
     vscode.commands.registerCommand("activeUsers.refresh", (data) => {
@@ -99,6 +234,10 @@ const constructViewPanel = (context) => {
     "activeUsers",
     ActiveUsersTreeDataProvider(users)
   );
+
+  const provider = SharingManagementWebview(context.extensionUri);
+
+  vscode.window.registerWebviewViewProvider(provider.viewType, provider);
 };
 
 module.exports = {
