e94e850fa6d38cb19cdaa8cf556689abcc99b065
unknown <lukasz.gras.97@gmail.com>
2021-08-07 21:38:23 +0200

    Fix

--- utils/runIOTests.js
+++ utils/runIOTests.js
@@ -141,10 +141,10 @@ const execFuncCpp = (inputType, inputValue, createdFromFile, outputType, outputV
       for(int i=0; i<n; i++){
         result += *(p + i);
         if(i != n-1){
-            result+=',';
+            result+=",";
         }
       }
-      result+='}';
+      result+="}";
       std::cout << result;
       `;
     }else if(inputType.includes("[]")) {
